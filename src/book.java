44a233a34461a37279f37f45573bda6e6349f318
Shawn H <shherubin@gmail.com>
2022-05-11 13:01:42 -0500

    logic and build help

--- src/book.java
+++ src/book.java
@@ -2,33 +2,17 @@ public class book {
     private String title;
     private String author;
     private boolean availablity;
+    private int bookID;
 
-    public String getTitle() {
-        return title;
-    } public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void getAuthor() {
-        return String.author;
-    } public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void getAvailability() {
-        return boolean.availability;
-    } public void setAvailability(boolean availability) {
-        this.availablity = availability;
-    }
-
-    public book(String title, String author, boolean availability) {
+    public book(String title, String author, boolean availability, int bookID) {
         this.title = title;
         this.author = author;
         this.availablity = availability;
+        this.bookID = bookID;
     }
     @Override
     public String toString() {
-        return title + "written by " + author + ", this book is " + availablity;
+        return title + "written by " + author + ", this book is " + availablity + "and has ID#" + bookID;
     }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
 
-        library.mangalibrary();
+        library.runMangalibrary();
     }
 }
